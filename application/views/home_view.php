91ffb59a98cff48723325764f0e4524360e3f4e0
PauloBaronista <paulo@rpm.com.br>
2021-10-02 14:48:37 -0300

    Ajuste layout

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -174,8 +174,8 @@
         <div class="row">
             <div class="col-xs-12 col-sm-12 col-md-10 col-md-offset-1 col-lg-10 col-lg-offset-1 padding-off box-1">
                 <div class="col-xs-12 col-sm-12 col-md-12">
-                    <p class="t-2">FALE COM AGENTE.<br>
-                        SÃO<span>18ANOS</span> SERVINDO <span>NOVAS IDEIAS</span>.</p>
+                    <p class="t-2">FALE COM A GENTE.<br>
+                        SÃO<span>18 ANOS</span> SERVINDO <span>NOVAS IDEIAS</span>.</p>
                 </div>
             </div>
             <div class="col-xs-12 col-sm-12 col-md-12 redes">
